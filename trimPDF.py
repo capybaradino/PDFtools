a7f23fe3c99daec6ce97584ee17fff64d0e471a9
Capybaradino <20378644+capybaradino@users.noreply.github.com>
2024-05-30 14:20:26 +0000

    A4/B4が選択できるように修正

--- trimPDF.py
+++ trimPDF.py
@@ -1,33 +1,48 @@
-# A4サイズのPDFの左上端からB4サイズ部分を切り出して新しいPDFを作成する
+# PDFの左上端から指定サイズ部分を切り出して新しいPDFを作成する
 # 入力するPDFと出力するPDFのファイル名はコマンドライン引数で指定する
-# 例: python trimB4fromA4.py input.pdf output.pdf
+# トリミングするサイズもコマンドライン引数で指定する(デフォルトはA4)
+# 例: python trimB4fromA4.py input.pdf A4
 
 import sys
 
 from PyPDF2 import PdfReader, PdfWriter
 
 
-def trimB4fromA4(inputFile, outputFile):
+def trimpdf(inputFile, outputFile, size):
     reader = PdfReader(inputFile)
     writer = PdfWriter()
+    if size == "B4":
+        xlen = 738
+        ylen = 1030
+    else:
+        xlen = 595
+        ylen = 842
+
     for page in reader.pages:
         # 原稿の左下と右上の座標を取得（用紙サイズ）
-        x0 = page.mediabox.left
-        y0 = page.mediabox.bottom
-        x1 = page.mediabox.right
+        # x0 = page.mediabox.left
+        # y0 = page.mediabox.bottom
+        # x1 = page.mediabox.right
         y1: float = page.mediabox.top
         # Update the page size to B4
-        page.mediabox.lower_left = (0, y1 - 1030)
-        page.mediabox.upper_right = (738, y1)
+        page.mediabox.lower_left = (0, y1 - ylen)
+        page.mediabox.upper_right = (xlen, y1)
         writer.add_page(page)
     with open(outputFile, "wb") as out:
         writer.write(out)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python trimB4fromA4.py input.pdf output.pdf")
+    if len(sys.argv) < 2:
+        print("Usage: python trimPDF.py input.pdf [A4/B4]")
         sys.exit()
-    trimB4fromA4(sys.argv[1], sys.argv[2])
+    inputFile = sys.argv[1]
+    outputFile = inputFile.replace(".pdf", "_trim.pdf")
+    if len(sys.argv) > 2:
+        size = sys.argv[2]
+    else:
+        size = "A4"
+
+    trimpdf(inputFile, outputFile, size)
 
 # 以上
